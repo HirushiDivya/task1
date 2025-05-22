f7a8acb401b88858fd4b6a84576a3e485e841799
HirushiDivya <shageekahirushi@gmail.com>
2025-05-22 12:06:16 +0530

    add_herosectioninto_app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import React from 'react';
 import './components/Header';
+import './components/HeroSection'
+import HeroSection from './components/HeroSection';
 
 function App() {
   return (
     <div className="font-sans">
       <Header />
+      <HeroSection />
     </div>
   );
 }
